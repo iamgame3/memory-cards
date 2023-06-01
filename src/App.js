2f97e61f09004981731fea1575abc6bd56be7124
iamgame3 <kosmosworld9@hotmail.com>
2023-06-01 18:57:00 -0400

    Remove CRA syntax from html, add a font, title, and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
+import React from "react";
+import Title from "./title";
 
 function App() {
-  return <>
-  </>;
+  return <Title />;
 }
 
 export default App;
---
+++ src/title.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-filename-extension */
+
+import React from "react";
+
+function Title() {
+  return (
+    <div className="title-container">
+      <h1 className="title">DBZ Memory Cards</h1>
+      <h3 className="subtitle">
+        Get points by clicking on an image, but don&apos;t click on the same
+        image more than once.
+      </h3>
+    </div>
+  );
+}
+
+export default Title;
